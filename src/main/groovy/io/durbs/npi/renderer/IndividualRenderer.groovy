34ffa461414c3e93f46ac6e27259af4081118622
Josh Durbin <durbinjo593@gmail.com>
2015-10-15 11:23:20 -0700

    Added static compilation

--- src/main/groovy/io/durbs/npi/renderer/IndividualRenderer.groovy
+++ src/main/groovy/io/durbs/npi/renderer/IndividualRenderer.groovy
@@ -1,10 +1,12 @@
 package io.durbs.npi.renderer
 
+import groovy.transform.CompileStatic
 import io.durbs.npi.domain.Individual
 import ratpack.groovy.handling.GroovyContext
 import ratpack.groovy.render.GroovyRendererSupport
 import ratpack.jackson.Jackson
 
+@CompileStatic
 class IndividualRenderer extends GroovyRendererSupport<Individual> {
 
   @Override
--- src/main/groovy/io/durbs/npi/renderer/OrganizationRenderer.groovy
+++ src/main/groovy/io/durbs/npi/renderer/OrganizationRenderer.groovy
@@ -1,10 +1,12 @@
 package io.durbs.npi.renderer
 
+import groovy.transform.CompileStatic
 import io.durbs.npi.domain.Organization
 import ratpack.groovy.handling.GroovyContext
 import ratpack.groovy.render.GroovyRendererSupport
 import ratpack.jackson.Jackson
 
+@CompileStatic
 class OrganizationRenderer extends GroovyRendererSupport<Organization> {
 
   @Override
